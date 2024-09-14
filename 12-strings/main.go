2581e27e2289c7ee5979271e6ffacbe2fd1d264a
itsparasbisht <iamparasbisht@gmail.com>
2024-09-14 20:47:08 +0530

    strings & runes in golang

---
+++ 12-strings/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// interpreted string
+	txt := "harry\nruns🏃"
+	fmt.Println(txt)
+
+	// raw sting
+	query := `harry \n runs`
+	fmt.Println(query)
+
+	// this will return the total number of bytes
+	fmt.Println(len(txt))
+
+	fmt.Println(txt[0])
+
+	// A rune in Go is an alias for the int32 type and represents a Unicode code point
+	// A rune literal in Go is a single character enclosed in single quotes
+	r := 'A'
+	fmt.Println(r)   // prints 65, unicode for A
+
+	// modifying a string using rune
+	txt2 := []rune(txt)
+	fmt.Println((txt2))
+	txt2[0] = 'B'
+	str := string(txt2)
+	fmt.Println(str)
+	
+}
